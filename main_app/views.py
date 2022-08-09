7ca592d5b2f974ecaa8ab89a97ed807405664412
adamascencio <ascencioadam@gmail.com>
2022-08-08 22:33:47 -0700

    add show functionality to show individual pokemon

--- main_app/views.py
+++ main_app/views.py
@@ -10,3 +10,7 @@ def about(request):
 def pokemon_index(request):
   p = Pokemon.objects.all()
   return render(request, 'pokemon/index.html', {'pokemon': p})
+
+def pokemon_detail(request, pokemon_id):
+  p = Pokemon.objects.get(id=pokemon_id)
+  return render(request, 'pokemon/detail.html', {'pokemon': p})
