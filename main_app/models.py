d85b4e9152e24b58a5d3106577c612bb85c2bd25
adamascencio <ascencioadam@gmail.com>
2022-08-09 22:51:42 -0700

    added Item model to models.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,15 +1,41 @@
-from django.db import models 
+from django.db import models
 from django.urls import reverse
 
+ITEMS = (
+  ('PT', 'Potion'),
+  ('SP', 'Super Potion'),
+  ('HP', 'Hyper Potion'),
+  ('RC', 'Rare Candy'),
+  ('RV', 'Revive'),
+)
+
 # Create your models here.
+
 class Pokemon(models.Model):
   name = models.CharField(max_length=255)
   type = models.CharField(max_length=255)
   super_effective = models.TextField(max_length=250)
   weak_against = models.TextField(max_length=250)
-  
+
   def __repr__(self):
-    return f'{self.name} - a {self.type} type pokemon'
+      return f'{self.name} - a {self.type} type pokemon'
 
   def get_absolute_url(self):
-    return reverse('detail', kwargs={'pokemon_id': self.id})
+      return reverse('detail', kwargs={'pokemon_id': self.id})
+
+
+class Item(models.Model):
+  date = models.DateField()
+  item = models.CharField(
+      'Item Given',
+      max_length=2,
+      choices=ITEMS,
+  )
+  # Create a pokemon_id foreign key
+  pokemon = models.ForeignKey(
+    Pokemon, 
+    on_delete=models.CASCADE
+    )
+
+  def __str__(self):
+      return f'{self.get_item_display()} on {self.date}'
